929e2cb3c9fc91cab9534e9d7b3539b1a422b667
Cuong Truong <tduccuong@gmail.com>
2025-01-17 12:01:49 +0100

    manually load nif

--- lib/ex_brotli.ex
+++ lib/ex_brotli.ex
@@ -1,32 +1,23 @@
 defmodule ExBrotli do
   @compile {:autoload, false}
-  @on_load {:init, 0}
 
-  def init() do
-    case load_nif() do
-      :ok ->
-        :ok
-      exp ->
-        raise "failed to load NIF: #{inspect exp}"
+  def ensure_loaded(so_file) do
+    case :erlang.load_nif(so_file, 0) do
+      :ok         -> {:ok, :loaded};
+      {:error, e} -> {:error, e}
     end
   end
+
   def decompress!(data) do
     case decompress_nif(data) do
-      {:ok, plain} ->
-        plain
-        {:error, reason} ->
-          throw({:error, reason})
+      {:ok, plain}     -> plain
+      {:error, reason} -> throw({:error, reason})
     end
   end
-  def decompress(data) do
-    decompress_nif(data)
-  end
+
+  def decompress(data), do: decompress_nif(data)
 
   # NIF
   def decompress_nif(_data), do: :erlang.nif_error(:nif_not_loaded)
-
-  defp load_nif() do
-    path = :filename.join(:code.priv_dir(:ex_brotli), 'brotli_nif')
-    :erlang.load_nif(path, 0)
-  end
+  
 end
